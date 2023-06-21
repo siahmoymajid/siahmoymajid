a117c0152c331571ff84baf6238e5ee6e086e2eb
siahmoymajid <122416542+siahmoymajid@users.noreply.github.com>
2023-06-20 19:36:06 -0700

    update package and deploy build

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -9,7 +9,7 @@ const Resume = ({ data, title }) => {
       <Row className="d-flex justify-content-between align-items-center w-100 mt-2">
         <Col md={4}>
           <a
-            href="http://"
+            href="/public/resume.pdf"
             target="_blank"
             rel="noopener noreferrer"
             className="display pop btn btn-light btn-lg"
